b1b032f9a32b66704e73bc03ffe70d572adeeda2
Juan Escobar <37384155+jcescobarn@users.noreply.github.com>
2025-06-17 19:30:52 -0500

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="milib-ejemplo",
-    version="0.0.1",
+    version="0.0.2",
     author="name",
     author_email="tuemail@ejemplo.com",
     description="Una librería de prueba para subir a Nexus",
